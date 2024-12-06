1cbc2b79b5cf3ba88df223d2f353e30661169b4d
Patrick Rakowicz <patrick.rakowicz@gmail.com>
2024-12-05 20:38:19 -0700

    Code builds, nothing is being read

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,9 @@ use dht11::Dht11;
 use esp_backtrace as _;
 use esp_hal::{
     delay::Delay,
-    gpio::{Io, Level, Output},
+    gpio::{Io, Level, Output, Flex},
     prelude::*,
 };
-use esp_hal::gpio::Input;
 
 #[entry]
 fn main() -> ! {
@@ -20,11 +19,27 @@ fn main() -> ! {
 
     // Set GPIO0 as an output, and set its state high initially.
     let mut led = Output::new(peripherals.GPIO4, Level::High);
+    let dht11_pin = Flex::new(peripherals.GPIO0);
+    let mut dht11 = Dht11::new(dht11_pin);
 
     let delay = Delay::new();
+    let mut dht11_delay = Delay::new();
 
     loop {
         led.toggle();
         delay.delay_millis(3000);
+
+        match dht11.perform_measurement(&mut dht11_delay) {
+            Ok(sensor_data) => {
+                esp_println::println!(
+                    "Temperature: {}°C, Humidity: {}%",
+                    sensor_data.temperature,
+                    sensor_data.humidity
+                );
+            }
+            Err(e) => {
+                esp_println::println!("Error reading from DHT11: {:?}", e);
+            }
+        }
     }
 }
