e6ad33726b78f5c8204ba97e74291d7092962dfd
Patrick Rakowicz <patrick.rakowicz@gmail.com>
2024-12-10 21:20:38 -0700

    working with hello rust, hello world

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use esp_backtrace as _;
 use esp_hal::{
     delay::Delay,
     gpio::{Io, Level, Output, Flex},
-    i2c::{I2C, MasterPins, MasterConfig},
+    i2c::master::{I2c, Config as I2cConfig},
     peripherals::Peripherals,
     prelude::*,
+    Config
 };
-
 use embedded_graphics::{
     pixelcolor::BinaryColor,
     prelude::*,
@@ -17,13 +17,14 @@ use embedded_graphics::{
     mono_font::{ascii::FONT_6X9, MonoTextStyleBuilder},
     text::Text,
 };
-use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
-
+use embedded_graphics::mono_font::ascii::FONT_6X10;
+use embedded_graphics::text::Baseline;
+use ssd1306::{mode::BufferedGraphicsMode, prelude::*, I2CDisplayInterface, Ssd1306};
 
 #[entry]
 fn main() -> ! {
     let peripherals = esp_hal::init({
-        let mut config = esp_hal::Config::default();
+        let mut config = Config::default();
         config.cpu_clock = CpuClock::max();
         config
     });
@@ -31,16 +32,39 @@ fn main() -> ! {
     // Set GPIO0 as an output, and set its state high initially.
     let mut led = Output::new(peripherals.GPIO4, Level::High);
 
-    let i2c_pins = MasterPins {
-        sda: peripherals.GPIO22,
-        scl: peripherals.GPIO21,
-    };
+    // Ic2 Display https://docs.rs/ssd1306/0.9.0/ssd1306/index.html
 
-    /// DHT11 Sensor needs
+    let i2c = I2c::new(peripherals.I2C0, I2cConfig::default())
+        .with_sda(peripherals.GPIO21)
+        .with_scl(peripherals.GPIO22);
+
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306::new(
+        interface,
+        DisplaySize128x64,
+        DisplayRotation::Rotate0,
+    ).into_buffered_graphics_mode();
+    display.init().unwrap();
+
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
+
+    Text::with_baseline("Hello world!", Point::zero(), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+
+    Text::with_baseline("Hello Rust!", Point::new(0, 16), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+
+    display.flush().unwrap();
+
+    // DHT11 Sensor needs
     // let dht11_pin = Flex::new(peripherals.GPIO0);
     // let mut dht11 = Dht11::new(dht11_pin);
     // let mut dht11_delay = Delay::new();
-
     let delay = Delay::new();
 
     loop {
